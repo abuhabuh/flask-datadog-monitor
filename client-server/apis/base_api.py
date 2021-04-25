73902c98eda15df312701a11e95f061134c1e671
John Wang <jue@alumni.cmu.edu>
2021-04-25 15:37:14 -0400

    add endpoint

--- client-server/apis/base_api.py
+++ client-server/apis/base_api.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 
 import flask
@@ -5,11 +6,17 @@ import flask
 
 def add_endpoints(flask_app, jinja_env):
 
-    @flask_app.route('/health', methods=['GET'])
-    def get_health():
-        return json.dumps({'status': 'ok'})
-
     @flask_app.route('/', methods=['GET'])
     def get_root():
         return jinja_env.get_template('index.html').render()
+
+    @flask_app.route('/date', methods=['GET'])
+    def get_date():
+        return json.dumps({
+            'date':  str(datetime.datetime.now().date())
+        })
+
+    @flask_app.route('/health', methods=['GET'])
+    def get_health():
+        return json.dumps({'status': 'ok'})
 
