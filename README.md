2f92932ce34c1bd20e4bb843a38a8596b18aac2d
John Wang <jue@alumni.cmu.edu>
2021-04-25 17:19:57 -0400

    Basic endpoint read and template gen working

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# Issues
+
+Need to be in the same venv as the target app we are generating for due to
+import dependencies. i.e. need to source venv of target app
+
+
 # Overview
 
 Utility for auto generating terraform monitor specifications from endpoints.
--- monitor-generator/flask_endpoint_parser.py
+++ monitor-generator/flask_endpoint_parser.py
@@ -1,14 +1,38 @@
 import importlib
+import os
 import sys
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-def _flask_app_from_module(module_name: str):
-    return importlib.import_module(module_name).app
+jinja_env = Environment(
+    loader=FileSystemLoader(
+        'templates'
+    ),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+
+def _flask_app_from_location(module_name: str):
+    """
+    :param module_name: String specifying path and module name as well as
+      actual flask app attribute. e.g., /path/to/module:flask_app
+    """
+    module_and_app_name: str = (module_name.split('/')[-1])
+    module_file: str = module_and_app_name.split(':')[0]
+    flask_app_obj: str = module_and_app_name.split(':')[1]
+
+    path = '/'.join(module_name.split('/')[0:-1])
+    sys.path.append(path)
+
+    flask_app_module = importlib.import_module(module_file)
+
+    return getattr(flask_app_module, flask_app_obj)
 
 
 def main():
-    module_name = sys.argv[1]
-    flask_app = _flask_app_from_module(module_name)
+    app_location: str = sys.argv[1]
+
+    flask_app = _flask_app_from_location(app_location)
 
     endpoint = ''
     methods = []
@@ -23,6 +47,13 @@ def main():
 
     print(endpoint)
     print(methods)
+    print(jinja_env.get_template('datadog_monitor.tmpl').render(
+        monitor_name='foo',
+        monitor_name_pretty='foo me once',
+        msg='asdfafd',
+        escalation_msg='escalate_asdfasdf',
+        monitor_query='select *',
+    ))
 
 
 if __name__ == '__main__':
