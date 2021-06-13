4f2d395d5e182d2b049afa829b08cb054e2be14f
John Wang <jue@alumni.cmu.edu>
2021-06-12 20:47:20 -0400

    WIP: console script

---
+++ flask_datadog/scripts/README.md
@@ -0,0 +1,3 @@
+# Overview
+
+Commandline executables
---
+++ flask_datadog/scripts/cmd_line.py
@@ -0,0 +1,9 @@
+"""
+Commandline functionality for package
+
+e.g., Generate terraform templates
+"""
+
+def main():
+    print('cmd line did something')
+
